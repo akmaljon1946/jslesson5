b795822edf51717239ed21624f7373680190eb1c
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-04-03 18:46:24 +0500

    add js5

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("home").innerHTML =
+Math.floor(Math.random() * 100);
+
+
+let a = Math.round(Math.random() * 100)
+
+if (a > 1 && a <= 17) {
+  
+  console.log("Maktab yoshi" ,` ${17} yoshgacha` );
+}
+else if (a > 18 && a <= 24) {
+
+  console.log("Universitet yoshi" ,` ${24} yoshgacha` );
+}
+else if (a > 25 && a <= 40) {
+  console.log("Ishlash yoshi",` ${40} yoshgacha` );
+}
+else if (a > 41) {
+  console.log("Nuroniylar yoshi",`${41} yoshdan yuqorilar` );
+}
+  
